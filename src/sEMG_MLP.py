199cffe4b2b5b86be926a39ffd4762c9b879870b
Yixiang Gao <yixiangg310573@gmail.com>
2023-12-23 17:03:27 -0600

    add sEMG MLP initial script

--- src/sEMG_MLP.py
+++ src/sEMG_MLP.py
@@ -8,7 +8,6 @@ import wandb
 import torch
 
 from torch import nn
-from torch.utils.data import TensorDataset, DataLoader
 from tsai.all import *
 from fastai.callback.wandb import *
 from fastai.layers import *
@@ -17,21 +16,6 @@ from tqdm.notebook import trange, tqdm
 
 from util.sEMGFeatureLoader import sEMGFeatureDataset
 
-class FeatureMLP(nn.Module):
-  def __init__(self):
-    super(FeatureMLP, self).__init__()
-    self.flatten = Reshape(-1)
-    self.mlp = nn.ModuleList()
-    self.mlp.append(LinBnDrop(48,50,bn=True, p=0.1, act=get_act_fn(nn.ReLU(inplace=True)), lin_first=False))
-    self.mlp.append(LinBnDrop(50,50,bn=True, p=0.2, act=get_act_fn(nn.ReLU(inplace=True)), lin_first=False))
-    self.mlp.append(LinBnDrop(50,50,bn=True, p=0.2, act=get_act_fn(nn.ReLU(inplace=True)), lin_first=False))
-    self.head = nn.Sequential(LinBnDrop(50,2, bn=False, p=0))
-  
-  def forward(self, x):
-    x = self.flatten(x)
-    for mlp in self.mlp: x = mlp(x)
-    return self.head(x)
-  
 def LoadTrainTestFeatures(FEAT, LABEL, sub_test):
   # Load testing samples
   X_Test     = FEAT[sub_test,0]
@@ -58,15 +42,14 @@ def load_datafile(file):
   DATA_ALL = sio.loadmat(file)
   FEAT_N           = DATA_ALL['FEAT_N']            # Normalized features
   LABEL            = DATA_ALL['LABEL']             # Labels
-  VOWEL            = DATA_ALL['LABEL_VOWEL']       # Type of Vowels
   VFI_1            = DATA_ALL['SUBJECT_VFI']       # VFI-1 Score
   SUBJECT_ID       = DATA_ALL['SUBJECT_ID']        # Sujbect ID
   SUBJECT_SKINFOLD = DATA_ALL['SUBJECT_SKINFOLD']  # Subject Skinfold Thickness
-  return FEAT_N, LABEL, SUBJECT_SKINFOLD, VOWEL, VFI_1, SUBJECT_ID
+  return FEAT_N, LABEL, SUBJECT_SKINFOLD, VFI_1, SUBJECT_ID
 
 # environment variable for the experiment
 WANDB = os.getenv("WANDB", False)
-GROUP = os.getenv("GROUP", "tests")
+GROUP = os.getenv("GROUP", "MLP-sEMG")
 
 if __name__ == "__main__":
   # X - FEAT_N
@@ -74,41 +57,35 @@ if __name__ == "__main__":
   # C - SUBJECT_SKINFOLD
   FEAT_N, LABEL, _, VFI_1, SUBJECT_ID = load_datafile("data/subjects_40_v6")
 
-  for sub_test in range(10):
+  # NOTE
+  # For the neural networks implementation, a high-level API was used in order to minimize implementation
+  # more reference can be found in https://timeseriesai.github.io/tsai/
+  for sub_test in range(5):
     sub_txt = "R%03d"%(int(SUBJECT_ID[sub_test][0][0]))
-    print('Test Subject %s:'%(sub_txt))
-    print('VFI-1:', (VFI_1[sub_test][0][0]))
-    # if int(VFI_1[sub_test][0][0]) > 10:
-    #   sub_group = 'Fatigued'
-    # else:
-    #   sub_group = 'Healthy'
+    sub_group = 'Fatigued' if int(VFI_1[sub_test][0][0][0]) > 10 else 'Healthy'
+    print(f"Test Subject {sub_txt}:")
+    print(f"VFI-1: {VFI_1[sub_test][0][0][0]}")
 
-    # if WANDB:
-    #     run = wandb.init(project="sEMG Leave-One-Out Classification 40 100 Feature Base TSAI",
-    #                      group=sub_group,
-    #                      name=sub_txt
-    #                      reinit=True,
-    #                      )
+    cbs = None
+    if WANDB:
+      run = wandb.init(project="Confounding-Mitigation-In-Deep-Learning",
+                       group=GROUP,
+                       name=sub_txt,
+                       tags=[sub_group],
+                       reinit=True)
+      cbs = WandbCallback()
 
-    # #  Load training/testing features
-    # X_Train, Y_Train, X_Test, Y_Test = LoadTrainTestFeatures(FEAT_N, LABEL, sub_test)
+    #  Load training and testing features
+    X_Train, Y_Train, X_Test, Y_Test = LoadTrainTestFeatures(FEAT_N, LABEL, sub_test)
 
-    # #  Data initialization for fastai/pytorch
-    # splits = get_splits(Y_Train, valid_size=.1, stratify=True, random_state=23, shuffle=True, show_plot=False)
-    # tfms  = [None, [Categorize()]]
-    # dsets = TSDatasets(X_Train, Y_Train, tfms=tfms, splits=splits)
+    # Setting "stratify" to True ensures that the relative class frequencies are approximately preserved in each train and validation fold.
+    splits = get_splits(Y_Train, valid_size=.1, stratify=True, random_state=23, shuffle=True, show_plot=False)
+    tfms  = [None, [Categorize()]]
+    dsets = TSDatasets(X_Train, Y_Train, tfms=tfms, splits=splits)
+    dls = TSDataLoaders.from_dsets(dsets.train, dsets.valid, shuffle_train=True, bs=32, num_workers=0)
+    model = MLP(c_in=dls.vars, c_out=dls.c, seq_len=48, layers=[50, 50, 50], use_bn=True)
+    print(model)
 
-    # dls = TSDataLoaders.from_dsets(dsets.train,
-    #                                dsets.valid,
-    #                                shuffle_train=True,
-    #                                bs=32,
-    #                                num_workers=0)
-
-    # # dls.show_batch()
-
-    # #  Model definitions
-    # model = FeatureMLP()
-
-    # if wand_config == 1:
-    #     learn = Learner(dls, model, loss_func=CrossEntropyLossFlat(), metrics=accuracy, cbs=WandbCallback())
-    # else:
+    learn = Learner(dls, model, loss_func=CrossEntropyLossFlat(), metrics=accuracy, cbs=cbs)
+    learn.lr_find()
+    learn.fit_one_cycle(50, lr_max=1e-3)
