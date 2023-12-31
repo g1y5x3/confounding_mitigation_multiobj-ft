dddd4bc48b6573f78de54dfae260f45350ee594d
Yixiang Gao <yixiangg310573@gmail.com>
2023-12-31 09:48:35 -0600

    disabled cpt loss, need to fix q(c|x) estimation

--- src/sEMG_MLP-CPT.py
+++ src/sEMG_MLP-CPT.py
@@ -14,7 +14,7 @@ from sklearn.metrics import accuracy_score
 from mlconfound.stats import partial_confound_test
 #
 from util.sEMGhelpers import load_datafile, partition
-from cpt import conditional_log_likelihood, generate_X_CPT_MC
+from cpt import conditional_log_likelihood, generate_X_CPT_MC, cpt_p_pearson
 
 class sEMGDataset(Dataset):
   def __init__(self, X_Train, Y_Train, C_Train=None):
@@ -45,10 +45,48 @@ class CrossEntropyLoss(nn.Module):
   def __init__(self):
     super().__init__()
 
-  def forward(self, yhat_c, y):
+  def __call__(self, yhat_c, y):
+    print(yhat_c)
+    print(y)
     yhat, _ = yhat_c
     return F.cross_entropy(yhat, y)
 
+# TODO vectorize this implementation
+# https://github.com/zhenxingjian/Partial_Distance_Correlation/blob/b088801996acefe38a67dff59bb8cbe3b20c7d91/Partial_Distance_Correlation.ipynb
+def distance_correlation(c, y):
+  matrix_a = torch.sqrt(torch.sum(torch.square(c.unsqueeze(0) - c.unsqueeze(1)), dim = -1) + 1e-12)
+  matrix_b = torch.sqrt(torch.sum(torch.square(y.unsqueeze(0) - y.unsqueeze(1)), dim = -1) + 1e-12)
+
+  matrix_A = matrix_a - torch.mean(matrix_a, dim = 0, keepdims= True) - torch.mean(matrix_a, dim = 1, keepdims= True) + torch.mean(matrix_a)
+  matrix_B = matrix_b - torch.mean(matrix_b, dim = 0, keepdims= True) - torch.mean(matrix_b, dim = 1, keepdims= True) + torch.mean(matrix_b)
+
+  gamma_XY = torch.sum(matrix_A * matrix_B)/ (matrix_A.shape[0] * matrix_A.shape[1])
+  gamma_XX = torch.sum(matrix_A * matrix_A)/ (matrix_A.shape[0] * matrix_A.shape[1])
+  gamma_YY = torch.sum(matrix_B * matrix_B)/ (matrix_A.shape[0] * matrix_A.shape[1])
+
+  correlation_r = gamma_XY/torch.sqrt(gamma_XX * gamma_YY + 1e-9)
+  return correlation_r
+
+def cpt_p_dcor(c, yhat, y, mcmc_steps=50, random_state=123, num_perm=1000):
+  # sampling permutations of c
+  bs = y.shape[0]
+  cond_log_like_mat = conditional_log_likelihood(X=c.numpy(), C=y.numpy(), xdtype='categorical')
+  Pi_init = generate_X_CPT_MC(mcmc_steps*5, cond_log_like_mat, np.arange(bs, dtype=int), random_state)
+
+  def workhorse(c, _random_state):
+    # batched os job_batch for efficient parallelization
+    Pi = generate_X_CPT_MC(mcmc_steps, cond_log_like_mat, Pi_init, random_state=_random_state)
+    return c[Pi]
+  rng = np.random.default_rng(random_state)
+  random_states = rng.integers(np.iinfo(np.int32).max, size=num_perm)
+  c_pi = torch.tensor(np.array(Parallel(n_jobs=-1)(delayed(workhorse)(c, i) for i in random_states)), dtype=torch.float32)
+  # compute p-value
+  t_yhat_c = distance_correlation(yhat.reshape([bs, -1]), c.reshape([bs, -1])).repeat(num_perm)
+  t_yhat_cpi = torch.zeros(num_perm)
+  for i in range(num_perm):
+    t_yhat_cpi[i] = distance_correlation(yhat.reshape([bs, -1]), c_pi[i,:].reshape([bs, -1]))
+
+  return torch.sigmoid(t_yhat_cpi - t_yhat_c).mean()
 
 # TODO how to make it faster?
 class CrossEntropyCPTLoss(nn.Module):
@@ -57,57 +95,27 @@ class CrossEntropyCPTLoss(nn.Module):
     self.mcmc_steps = mcmc_steps   # this is the default value used inside original CPT function
     self.random_state = random_state
     self.num_perm = num_perm
-    # ideally q(c|y) can be estimated here and use the corresponding log-likelihood when
-    # each batch is being fetched. however, the only technicial difficulty is that the dataloader
-    # will shuffle the entire data which makes it harder to track
-
-  def workhorse(self, c, _random_state):
-    # batched os job_batch for efficient parallelization
-    Pi = generate_X_CPT_MC(self.mcmc_steps, self.cond_log_like_mat, self.Pi_init, random_state=_random_state)
-    return c[Pi]
-
-  # TODO vectorize this implementation
-  # https://github.com/zhenxingjian/Partial_Distance_Correlation/blob/b088801996acefe38a67dff59bb8cbe3b20c7d91/Partial_Distance_Correlation.ipynb
-  def distance_correlation(self, c, y):
-    matrix_a = torch.sqrt(torch.sum(torch.square(c.unsqueeze(0) - c.unsqueeze(1)), dim = -1) + 1e-12)
-    matrix_b = torch.sqrt(torch.sum(torch.square(y.unsqueeze(0) - y.unsqueeze(1)), dim = -1) + 1e-12)
-
-    matrix_A = matrix_a - torch.mean(matrix_a, dim = 0, keepdims= True) - torch.mean(matrix_a, dim = 1, keepdims= True) + torch.mean(matrix_a)
-    matrix_B = matrix_b - torch.mean(matrix_b, dim = 0, keepdims= True) - torch.mean(matrix_b, dim = 1, keepdims= True) + torch.mean(matrix_b)
-
-    gamma_XY = torch.sum(matrix_A * matrix_B)/ (matrix_A.shape[0] * matrix_A.shape[1])
-    gamma_XX = torch.sum(matrix_A * matrix_A)/ (matrix_A.shape[0] * matrix_A.shape[1])
-    gamma_YY = torch.sum(matrix_B * matrix_B)/ (matrix_A.shape[0] * matrix_A.shape[1])
-
-    correlation_r = gamma_XY/torch.sqrt(gamma_XX * gamma_YY + 1e-9)
-    return correlation_r
-
-  def cpt_p(self, c, yhat, y):
-    bs = y.shape[0]
-    # estimating q(c|y)
-    self.cond_log_like_mat = conditional_log_likelihood(X=c.numpy(), C=y.numpy(), xdtype='categorical')
-    self.Pi_init = generate_X_CPT_MC(self.mcmc_steps*5, self.cond_log_like_mat, np.arange(bs, dtype=int), self.random_state)
-    # sampling permutations of c
-    rng = np.random.default_rng(self.random_state)
-    random_states = rng.integers(np.iinfo(np.int32).max, size=self.num_perm)
-    c_pi = torch.tensor(np.array(Parallel(n_jobs=-1)(delayed(self.workhorse)(c, i) for i in random_states)), dtype=torch.float32)
-    # compute p-value
-    t_yhat_c = self.distance_correlation(yhat.reshape([bs, -1]), c.reshape([bs, -1])).repeat(self.num_perm)
-    t_yhat_cpi = torch.zeros(self.num_perm)
-    for i in range(self.num_perm):
-      t_yhat_cpi[i] = self.distance_correlation(yhat.reshape([bs, -1]), c_pi[i,:].reshape([bs, -1]))
-
-    return torch.sigmoid(t_yhat_c - t_yhat_cpi).mean()
 
   def __call__(self, yhat_c, y):
     yhat, c = yhat_c
-    p = self.cpt_p(c, yhat, y)
-
-    return F.cross_entropy(yhat, y) + (1 - p)
+    # p = cpt_p(c, yhat, y, self.mcmc_steps, self.random_state, self.num_perm)
+    # return F.cross_entropy(yhat, y) - p
+    return F.cross_entropy(yhat, y)
 
 def accuracy(preds_confound, targets):
   preds, _, = preds_confound
   return (preds.argmax(dim=-1) == targets).float().mean()
+
+def pvalue_dcor(preds_confound, targets):
+  preds, confound = preds_confound
+  return cpt_p_dcor(confound, preds, targets)
+
+def pvalue_pearson(preds_confound, targets):
+  preds, confound = preds_confound
+  # print(preds.argmax(dim=1), confound, targets)
+  p, _ = cpt_p_pearson(confound.numpy(), preds.argmax(dim=1).numpy(), targets.numpy(), random_state=123, num_perm=1000, dtype='categorical')
+  # print(p)
+  return p
 
 # environment variable for the experiment
 WANDB = os.getenv("WANDB", False)
@@ -130,7 +138,7 @@ if __name__ == "__main__":
   train_acc = np.zeros(40)
   test_acc  = np.zeros(40)
   p_value   = np.zeros(40)
-  for sub_test in range(20, 21):
+  for sub_test in range(0, 1):
     sub_txt = "R%03d"%(int(SUBJECT_ID[sub_test][0][0]))
     sub_group = "Fatigued" if int(VFI_1[sub_test][0][0][0]) > 10 else "Healthy"
     print('\n===No.%d: %s===\n'%(sub_test+1, sub_txt))
@@ -145,6 +153,7 @@ if __name__ == "__main__":
     X_Train, Y_Train, C_Train, X_Test, Y_Test, C_Test = partition(FEAT_N, LABEL, SUBJECT_SKINFOLD, sub_test)
     # convert labels from [-1, 1] to [0, 1] so the probability density function estimation will be consistent with the dataset transformation
     Y_Train = np.where(Y_Train == -1, 0, 1)
+    Y_Test  = np.where(Y_Test == -1, 0, 1)
 
     # Setting "stratify" to True ensures that the relative class frequencies are approximately preserved in each train and validation fold.
     splits = get_splits(Y_Train, valid_size=.1, stratify=True, random_state=123, shuffle=True, show_plot=False)
@@ -159,7 +168,7 @@ if __name__ == "__main__":
     model = MLPC(c_in=1, c_out=2, seq_len=48, layers=[50, 50, 50], use_bn=True)
     print(model)
 
-    learn = Learner(dls, model, loss_func=CrossEntropyCPTLoss(), metrics=accuracy, cbs=cbs)
+    learn = Learner(dls, model, loss_func=CrossEntropyCPTLoss(), metrics=[accuracy, pvalue_dcor, pvalue_pearson], cbs=cbs)
 
     # Basically apply some tricks to make it converge faster
     # https://docs.fast.ai/callback.schedule.html#learner.lr_find
@@ -181,8 +190,15 @@ if __name__ == "__main__":
     p_value[sub_test] = ret.p
     print(f"P Value     : {p_value[sub_test]}")
 
+    # this is extremely slow, even on the gpu dute to computing the pairwise distance over a 6000+ training data size
+    # print(f"P Value (dCor): {cpt_p_dcor(c=train_c, yhat=train_preds, y=train_targets)}")
+
     # Testing accuracy
     dls_test = dls.new(dsets_test)
+    # the loss function is still being called internally during inference probably for some internal tracking in the API,
+    # hence here switch to a different loss function since there's no point calculating the cpt p value
+    learn.loss_func = CrossEntropyLoss()
+    learn.metrics = accuracy
     test_output, test_targets = learn.get_preds(dl=dls_test, with_loss=False)
     test_preds, test_c = test_output
     test_acc[sub_test] = accuracy_score(test_targets, test_preds.argmax(dim=1))
