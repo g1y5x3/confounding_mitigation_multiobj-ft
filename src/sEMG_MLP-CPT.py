e3f49e962ba04dc787c72067d793d43769abd82c
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-01-29 10:48:34 -0600

    modify dataloader to use custom sampler

--- src/sEMG_MLP-CPT.py
+++ src/sEMG_MLP-CPT.py
@@ -26,7 +26,7 @@ class sEMGSampler(Sampler):
     return len(self.sids)
 
   def __iter__(self):
-    return iter((0)*len(self.sids))
+    return iter([0]*len(self.sids))
 
 class sEMGDataset(Dataset):
   def __init__(self, X, Y, C, index, sid, train=False):
@@ -41,6 +41,7 @@ class sEMGDataset(Dataset):
     return len(self.Y)
 
   def __getitem__(self, idx):
+    print(idx)
     x = torch.tensor(self.X[idx,:], dtype=torch.float32)
     c = torch.tensor(self.C[idx],   dtype=torch.float32)
     i = torch.tensor(self.i[idx],   dtype=torch.int)
@@ -120,11 +121,8 @@ if __name__ == "__main__":
 
     bs = 256
     print(f"batch size: {bs}")
-    train_dl = DataLoader(dsets_train, batch_size=bs, sampler=train_sampler, num_workers=4, pin_memory=True)
-    valid_dl = DataLoader(dsets_valid, batch_size=bs, shuffle=False, num_workers=4, pin_memory=True)
-    test_dl  = DataLoader(dsets_test, batch_size=bs, shuffle=False, num_workers=4, pin_memory=True)
 
-    dls = DataLoaders(train_dl, valid_dl)
+    dls = DataLoaders.from_dsets(dsets_train, dsets_valid, shuffle=False, bs=bs, sampler=train_sampler)
 
     # This model is pre-defined in https://timeseriesai.github.io/tsai/models.mlp.html
     model = MLPC(c_in=1, c_out=2, seq_len=48, layers=[50, 50, 50], use_bn=True)
@@ -132,7 +130,7 @@ if __name__ == "__main__":
 
     # main training loop is abstracted
     learn.lr_find()
-    learn.fit_one_cycle(50, lr_max=1e-3)
+    learn.fit_one_cycle(1, lr_max=1e-3)
 
     # Training accuracy
     train_output, train_targets = learn.get_preds(dl=dls.train, with_loss=False)
