984ac91bcf8dfc097a427f876a5f08c0197128da
Yixiang Gao <yg5d6@mail.missouri.edu>
2024-01-29 15:07:48 -0600

    disable eval test and debug custom sampler

--- src/sEMG_MLP-CPT.py
+++ src/sEMG_MLP-CPT.py
@@ -4,29 +4,18 @@ import numpy as np
 import torch
 import torch.nn.functional as F
 from torch import nn
-from torch.utils.data import Sampler, Dataset
-from fastai.learner import Learner
-from fastai.data.core import DataLoader, DataLoaders
+from torch.utils.data import Dataset
 from fastai.callback.wandb import WandbCallback
+from fastai.data.core import DataLoader, DataLoaders
+from fastai.learner import Learner
 from tsai.all import get_splits, MLP
-from sklearn.metrics import accuracy_score
 from util.sEMGhelpers import load_datafile, partition
-from cpt import conditional_log_likelihood, cpt_p_pearson, cpt_p_pearson_torch
+from cpt import conditional_log_likelihood, cpt_p_pearson_torch
 
 # environment variable for the experiment
 WANDB = os.getenv("WANDB", False)
 NAME  = os.getenv("NAME",  "Confounding-Mitigation-In-Deep-Learning")
 GROUP = os.getenv("GROUP", "MLP-sEMG-CPT")
-
-class sEMGSampler(Sampler):
-  def __init__(self, sids):
-    self.sids = sids
-
-  def __len__(self):
-    return len(self.sids)
-
-  def __iter__(self):
-    return iter([0]*len(self.sids))
 
 class sEMGDataset(Dataset):
   def __init__(self, X, Y, C, index, sid, train=False):
@@ -37,22 +26,39 @@ class sEMGDataset(Dataset):
     self.sid = sid
     self.train = train
 
+    # using a custom sampler is way too much work at the moment, might as well
+    # generate the entire sequence all at once and just use the unshuffled index
+    # to pass along
+    if self.train:
+      # 1. calculate the total amount of batches that are needed
+      # 2. sample the indices of training data based on their subject IDs
+      self.idx = range(len(self.Y))
+      print(self.idx)
+
   def __len__(self):
     return len(self.Y)
 
   def __getitem__(self, idx):
-    print(idx)
-    x = torch.tensor(self.X[idx,:], dtype=torch.float32)
-    c = torch.tensor(self.C[idx],   dtype=torch.float32)
-    i = torch.tensor(self.i[idx],   dtype=torch.int)
-    y = torch.tensor(self.Y[idx],   dtype=torch.long)
+    # TODO
+    # when idx is 0 reset self.idx
+    if self.train:
+      x = torch.tensor(self.X[self.idx[idx],:], dtype=torch.float32)
+      c = torch.tensor(self.C[self.idx[idx]],   dtype=torch.float32)
+      i = torch.tensor(self.i[self.idx[idx]],   dtype=torch.int)
+      y = torch.tensor(self.Y[self.idx[idx]],   dtype=torch.long)
+    else:
+      x = torch.tensor(self.X[idx,:], dtype=torch.float32)
+      c = torch.tensor(self.C[idx],   dtype=torch.float32)
+      i = torch.tensor(self.i[idx],   dtype=torch.int)
+      y = torch.tensor(self.Y[idx],   dtype=torch.long)
     return (x, c, i), y
 
 # model is the same but passing an additional input variable
-class MLPC(MLP):
+class MLP_CPT(MLP):
   def __init__(self, c_in, c_out, seq_len, layers,
                ps=[0.1, 0.2, 0.2], act=nn.ReLU(inplace=True), use_bn=False, bn_final=False, lin_first=False, fc_dropout=0., y_range=None):
-    super().__init__(c_in, c_out, seq_len, layers, ps, act, use_bn, bn_final, lin_first, fc_dropout, y_range)
+    super().__init__(c_in, c_out, seq_len, layers,
+                     ps, act, use_bn, bn_final, lin_first, fc_dropout, y_range)
 
   def forward(self, x_c_idx):
     x, c, idx = x_c_idx
@@ -88,12 +94,15 @@ def accuracy(preds_confound_index, targets):
 
 
 if __name__ == "__main__":
+  # TODO
+  # add input argument for the experiments
   FEAT_N, LABEL, SUBJECT_SKINFOLD, VFI_1, SUBJECT_ID = load_datafile("data/subjects_40_v6")
 
   train_acc = np.zeros(40)
   valid_acc = np.zeros(40)
   test_acc  = np.zeros(40)
   p_value   = np.zeros(40)
+
   for sub_test in range(0, 1):
     sub_txt = "R%03d"%(int(SUBJECT_ID[sub_test][0][0]))
     sub_group = "Fatigued" if int(VFI_1[sub_test][0][0][0]) > 10 else "Healthy"
@@ -110,56 +119,56 @@ if __name__ == "__main__":
     Y_Train = np.where(Y_Train == -1, 0, 1)
     Y_Test  = np.where(Y_Test  == -1, 0, 1)
 
-    # Setting "stratify" to True ensures that the relative class frequencies are approximately preserved in each train and validation fold.
     cond_like_mat = conditional_log_likelihood(X=C_Train, C=Y_Train, xdtype='categorical')
-    splits = get_splits(Y_Train, valid_size=.1, stratify=True, random_state=123, shuffle=True, show_plot=False)
-    dsets_train = sEMGDataset(X_Train[splits[0],:], Y_Train[splits[0]], C_Train[splits[0]], splits[0], ID_Train[splits[0]])
-    dsets_valid = sEMGDataset(X_Train[splits[1],:], Y_Train[splits[1]], C_Train[splits[1]], splits[1], ID_Train[splits[1]])
-    dsets_test  = sEMGDataset(X_Test, Y_Test, C_Test, list(np.arange(len(X_Test))), ID_Test)
 
-    train_sampler = sEMGSampler(ID_Train[splits[0]])
+    # Setting "stratify" to True ensures that the relative class frequencies are approximately preserved in each train and validation fold
+    splits = get_splits(Y_Train, valid_size=.1, stratify=True, random_state=123, shuffle=True, show_plot=False)
+    dsets_train = sEMGDataset(X_Train[splits[0],:], Y_Train[splits[0]], C_Train[splits[0]], splits[0], ID_Train[splits[0]], train=True)
+    dsets_valid = sEMGDataset(X_Train[splits[1],:], Y_Train[splits[1]], C_Train[splits[1]], splits[1], ID_Train[splits[1]], train=False)
+    dsets_test  = sEMGDataset(X_Test, Y_Test, C_Test, list(np.arange(len(X_Test))), ID_Test)
 
     bs = 256
     print(f"batch size: {bs}")
 
-    dls = DataLoaders.from_dsets(dsets_train, dsets_valid, shuffle=False, bs=bs, sampler=train_sampler)
-
+    dl_train = DataLoader(dsets_train, shuffle=False, bs=bs, drop_last=True)
+    dl_valid = DataLoader(dsets_valid, shuffle=False, bs=bs)
+    dls = DataLoaders(dl_train, dl_valid)
     # This model is pre-defined in https://timeseriesai.github.io/tsai/models.mlp.html
-    model = MLPC(c_in=1, c_out=2, seq_len=48, layers=[50, 50, 50], use_bn=True)
+    model = MLP_CPT(c_in=1, c_out=2, seq_len=48, layers=[50, 50, 50], use_bn=True)
     learn = Learner(dls, model, loss_func=CrossEntropyCPTLoss(cond_like_mat), metrics=[accuracy], cbs=cbs)
 
-    # main training loop is abstracted
+    # # main training loop is abstracted
     learn.lr_find()
-    learn.fit_one_cycle(1, lr_max=1e-3)
+    learn.fit_one_cycle(50, lr_max=1e-3)
 
-    # Training accuracy
-    train_output, train_targets = learn.get_preds(dl=dls.train, with_loss=False)
-    train_preds, train_c, _ = train_output
-    train_acc[sub_test] = accuracy_score(train_targets, train_preds.argmax(dim=1))
-    print(f"Training acc   : {train_acc[sub_test]}")
+    # # Training accuracy
+    # train_output, train_targets = learn.get_preds(dl=dls.train, with_loss=False)
+    # train_preds, train_c, _ = train_output
+    # train_acc[sub_test] = accuracy_score(train_targets, train_preds.argmax(dim=1))
+    # print(f"Training acc   : {train_acc[sub_test]}")
 
-    # P-value (only makes sense to report for training)
-    p, _ = cpt_p_pearson(train_c.numpy(), train_preds.argmax(dim=1).numpy(), train_targets.numpy(), cond_like_mat[splits[0],:][:,splits[0]],
-                                mcmc_steps=100, random_state=None, num_perm=2000, dtype='categorical')
-    p_value[sub_test] = p
-    print(f"P Value        : {p}")
+    # # P-value (only makes sense to report for training)
+    # p, _ = cpt_p_pearson(train_c.numpy(), train_preds.argmax(dim=1).numpy(), train_targets.numpy(), cond_like_mat[splits[0],:][:,splits[0]],
+    #                             mcmc_steps=100, random_state=None, num_perm=2000, dtype='categorical')
+    # p_value[sub_test] = p
+    # print(f"P Value        : {p}")
 
-    # Validation accuracy
-    valid_output, valid_targets = learn.get_preds(dl=dls.valid, with_loss=False)
-    valid_preds, valid_c, _ = valid_output
-    valid_acc[sub_test] = accuracy_score(valid_targets, valid_preds.argmax(dim=1))
-    print(f"Validation acc : {valid_acc[sub_test]}")
+    # # Validation accuracy
+    # valid_output, valid_targets = learn.get_preds(dl=dls.valid, with_loss=False)
+    # valid_preds, valid_c, _ = valid_output
+    # valid_acc[sub_test] = accuracy_score(valid_targets, valid_preds.argmax(dim=1))
+    # print(f"Validation acc : {valid_acc[sub_test]}")
 
-    # Testing accuracy
-    dls_test = dls.new(dsets_test)
-    learn.loss_func = CrossEntropyLoss()
-    learn.metrics = accuracy
-    test_output, test_targets = learn.get_preds(dl=dls_test, with_loss=False)
-    test_preds, test_c, _ = test_output
-    test_acc[sub_test] = accuracy_score(test_targets, test_preds.argmax(dim=1))
-    print(f"Testing acc : {test_acc[sub_test]}")
+    # # Testing accuracy
+    # dls_test = dls.new(dsets_test)
+    # learn.loss_func = CrossEntropyLoss()
+    # learn.metrics = accuracy
+    # test_output, test_targets = learn.get_preds(dl=dls_test, with_loss=False)
+    # test_preds, test_c, _ = test_output
+    # test_acc[sub_test] = accuracy_score(test_targets, test_preds.argmax(dim=1))
+    # print(f"Testing acc : {test_acc[sub_test]}")
 
-    if WANDB: wandb.log({"subject_info/vfi_1" : int(VFI_1[sub_test][0][0]),
-                         "metrics/train_acc_opt"  : train_acc[sub_test],
-                         "metrics/test_acc_opt"   : test_acc[sub_test],
-                         "metrics/p_value_opt"    : p_value[sub_test]})
+    # if WANDB: wandb.log({"subject_info/vfi_1" : int(VFI_1[sub_test][0][0]),
+    #                      "metrics/train_acc_opt"  : train_acc[sub_test],
+    #                      "metrics/test_acc_opt"   : test_acc[sub_test],
+    #                      "metrics/p_value_opt"    : p_value[sub_test]})
